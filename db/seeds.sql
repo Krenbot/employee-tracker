7e020434de5f2ac465a37ecf218e57bc4b0d9ab0
Steven Krenn <stevekrenn@gmail.com>
2022-11-17 17:10:31 -0600

    added seeds to sql database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,18 @@
 INSERT INTO department (name)
-VALUES ('Engineering')
+VALUES ('Engineering'), ('Finance'), ('Sales'), ('Design');
+
+INSERT INTO role (title,salary, department_id )
+VALUES 
+('Engineer', 85000, 1)
+('Finance Manager', 75000, 2)
+('Salesperson', 99000, 3)
+('Designer', 75000, 4)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id);
+VALUES 
+('Steven', 'Krenn', 1, NULL)
+('Billy', 'Snipplets', 2, 1)
+('James', 'Cameron', 3, 1)
+('Josiah', 'Henderson', 4, 2)
+('Audra', 'Lindley', )
+('Jonathan', 'Lewis', 4, 2)
--- server.js
+++ server.js
@@ -1,9 +1,2 @@
 import mysql from 'mysql2'
 import inquirer from 'inquirer'
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: ''
-})
