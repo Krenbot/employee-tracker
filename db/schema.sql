030fc73ef3b610149c347365a1a5fdbc4f88c23e
Steven Krenn <stevekrenn@gmail.com>
2022-11-17 14:35:31 -0600

    added env to connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,24 @@
 DROP DATABSE IF EXISTS employees_db
 CREATE DATABASE employees_db
 
-USE employees_db
+USE employees_db;
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
-)
+);
 
-CREATE TABLE role(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    role_id INT NOT NULL AUTO_INCREMENT
-    manager_id INT
-    PRIMARY KEY(id)
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL AUTO_INCREMENT,
+    manager_id INT,
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO department (name)
+VALUES ('Engineering')
