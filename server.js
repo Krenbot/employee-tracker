5f0a2f744b8540edaa8f569e27c96f3f7d0df7b4
Steven Krenn <stevekrenn@gmail.com>
2022-11-18 13:05:11 -0600

    added console.table in package.json, added viewDepartments todo

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
 const inquirer = require('inquirer')
 const sql = require('mysql2')
 require('dotenv').config()
+const cTable = require('console.table');
 
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-
-const viewDepartments = async () => {}
+const viewDepartments = async () => {
+    THEN I am presented with a formatted table showing department names and department ids
+}
 
 const viewRoles = async () => {
-    // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 }
 
 const viewEmployees = async () => {
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 }
 
-const addDepartment = async () => {}
-
-const addRole = async () => {}
-
-const addEmployee = async () => {}
-
-const updateEmployee = async () => {}
-
-
-
-// WHEN I choose to view all employees
-
-
-// WHEN I choose to add a department
+const addDepartment = async () => {
 // THEN I am prompted to enter the name of the department and that department is added to the database
+}
 
-// WHEN I choose to add a role
+const addRole = async () => {
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+}
 
-// WHEN I choose to add an employee
+const addEmployee = async () => {
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+}
 
-// WHEN I choose to update an employee role
+const updateEmployee = async () => {
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+}
+
+const promptChoices = async () => {
+    const choices = await inquirer.prompt ([{
+        type: 'list',
+        message: 'Choose an action',
+        name: 'choices',
+        choices: ['VIEW all Departments', 'VIEW all Roles', 'VIEW all Employees', 'ADD a Department', "ADD a Role", 'ADD an Employee', "UPDATE Employees"]
+    }
+])
+}
