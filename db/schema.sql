37300431110ceb20f50e2f98680788c30e5747f3
Steve Krenn <stevekrenn@gmail.com>
2022-11-17 18:23:48 -0600

    seeds/schema syntax fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
-DROP DATABSE IF EXISTS employees_db
-CREATE DATABASE employees_db
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
+CREATE TABLE department(       
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT NOT NULL AUTO_INCREMENT,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL AUTO_INCREMENT,
-    manager_id INT,
+    role_id INT NOT NULL,
+    manager_id INT
 );
